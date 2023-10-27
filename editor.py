eedaffb50d02912266e50062c895f820c03ecfbe
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-10-27 18:23:20 +0800

    added the main file for the editor

---
+++ editor.py
@@ -0,0 +1,25 @@
+from rich import print
+from rich.layout import Layout
+from rich.console import Console
+from pynput import keyboard
+
+MIN_SIZE = 3
+
+def set_layout():
+	layout = Layout(name="root")
+
+	layout.split(
+		Layout(name="header", size=MIN_SIZE),
+		Layout(name="body", minimum_size=5),
+		Layout(name="footer", size=MIN_SIZE),
+	)
+
+	return layout
+
+def Editor(file: str):
+	editor = set_layout()
+
+	return
+
+if __name__ == "__main__":
+	print(set_layout())
