b2adf7041214790adb0da4228213cadd8a600b5f
Jigchu <111167590+Jigchu@users.noreply.github.com>
2023-11-01 20:59:01 +0800

    Made editor actuallt work

--- src/editor.py
+++ src/editor.py
@@ -1,18 +1,42 @@
+import sys
+import os
+
+from pynput import keyboard as kbd
+from rich.live import Live
+from rich import print
+
+from util.kbd_interrupt import DelayedKeyboardInterrupt
 from util.console import console
 from util.file import *
-import ui
+import ui.ui as ui
+from ui.body import *
 
 def Editor(file: File):
+	header = ui.Header(file.name)
+	body = Body(file)
+	footer = ui.Footer()
+
 	editor = ui.set_layout()
-	editor["header"].update(ui.Header(file.name))
-	editor["footer"].update(ui.Footer())
-	console.print(editor)
+	editor["header"].update(header)
+	editor["footer"].update(footer)
+	editor["body"].update(body)
 	
+	with Live(editor):
+		with kbd.Listener(on_press=body.on_press) as listener:
+			listener.join()
+
 	return 0
 
 if __name__ == "__main__":
-	editor = ui.set_layout()
-	editor["header"].update(ui.Header("editor.py"))
-	editor["footer"].update(ui.Footer())
-	editor["body"].update(ui.body(File("ui.py", "editor")))
-	console.print(editor)
+	try:
+		if len(sys.argv) != 2:
+			print("[bold red]:warning-emoji:  PROPER USAGE: python src/editor.py [FILE][/]")
+			sys.exit(1)
+		file_name = sys.argv[1]
+		p, root = os.path.split(os.getcwd())
+		file = File(file_name, root)
+		with DelayedKeyboardInterrupt():
+			Editor(file)
+			sys.exit(0)
+	except KeyboardInterrupt:
+		pass
